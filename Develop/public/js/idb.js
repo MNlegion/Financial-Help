cc0a1976bdbc268fa6572bbde3256a39bb0b2cbc
MNlegion <rkreuser30@gmail.com>
2022-07-13 11:34:00 -0500

    idb updates

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -54,9 +54,42 @@ function uploadTransaction() {
   
     // get all records from store and set to a variable
     const getAll = budgetObjectStore.getAll();
-
-
-    // getAll.onsuccess = function() {
+  
+    getAll.onsuccess = function() {
 
     // if there was data in indexedDb's store send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          // open one more transaction
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+          // access the new_transaction object store
+          const budgetObjectStore = transaction.objectStore('new_transaction');
+          
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert('All saved transactions has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
+  }
+};
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
